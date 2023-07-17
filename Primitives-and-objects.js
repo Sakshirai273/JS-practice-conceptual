d1e987c238b6f6bf094f725a14de445d04c18f0e
Sakshi <sakshirai273@gmail.com>
2023-07-17 17:53:15 +0530

    operators

--- Primitives-and-objects.js
+++ Primitives-and-objects.js
@@ -9,3 +9,22 @@
 
 // NON- PRIMITIVE DATA TYPES
 // Objects -> KEY - VALUE PAIRS 
+
+let a = null;
+let b= 345;
+let c= true;
+let d  = BigInt("57") +BigInt("3");
+let e = "sakshi"
+let f = Symbol("I'm symbol")
+let g = undefined
+console.log(a,b,c,d,c,f,g)
+console.log(typeof d)
+
+//objects in JS
+const item = {
+    "sakshi":true,
+    "shivam":false,
+    "lavi" : 57,
+    "rohan":undefined
+}
+console.log(item["sakshi"])
---
+++ arithmetic-operator.js
@@ -0,0 +1,28 @@
+//ARITHMETIC OPERATORS
+let a =45;
+let = 4;
+console.log("a + b=",a+b) //49
+console.log("a - b=",a-b) //41
+console.log("a / b=",a/b) //11.25
+console.log("a ** b=",a**b)  // a ki power b
+console.log("a % b=",a%b) //1
+console.log("++a=",++a) //46
+console.log("a ++ =",a++) //46
+console.log("--a=", --a) //46
+console.log("a --=", a--) //46
+console.log("a =", a) //45
+console.log("a--=", a--) //45
+
+//ASSIGNMENT OPERATORS
+let assignment = 1;
+assignment += 5 //same as a = a+5
+console.log(assignment)
+//same goes for other assignment operators
+
+//COMPARISON OPERATORS
+let comp1 = 6;
+let comp2 = "6";
+console.log ("comp1 == comp2" , comp1 == comp2) //true
+console.log("comp1 != comp2 is" , comp1 != comp2) //false
+console.log("comp1 === comp2 is" , comp1 === comp2) //false answer dega , value same hai lekin type alag hai 
+console.log("comp1 !== comp2 is" , comp1 !== comp2)//true
